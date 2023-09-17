2669bb44a15ca5cff1a9b414ed0d3c88b55225b9
VladislavVest <vladislavbrusencov@gmail.com>
2023-09-17 19:25:02 +0300

    create main.js

---
+++ public/main.js
@@ -0,0 +1,16 @@
+const log = console.log;
+
+/* <span class="material-symbols-outlined">
+elderly
+</span>
+
+<span class="material-symbols-outlined">
+  elderly_woman
+  </span> */
+
+  const socket = io();
+  log('hello');
+    // socket.emit("massage", "hellotest");
+    // // socket.on("data", (dt) => {
+    // //   alert(dt);
+    // // });
