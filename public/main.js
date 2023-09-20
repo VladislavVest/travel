6cc9f6929c120ca5fbe2fe50c9b5201505931ed4
VladislavVest <vladislavbrusencov@gmail.com>
2023-09-20 19:37:13 +0300

    add move

--- public/main.js
+++ public/main.js
@@ -2,9 +2,7 @@ const log = console.log;
 
 
 
-/* <span class="material-symbols-outlined">
-  elderly_woman
-  </span>  */
+
 
 const socket = io();
 // socket.emit("massage", "hellotest");
@@ -12,10 +10,72 @@ const socket = io();
 // //   alert(dt);
 // // });
 
+const gameField = [
+  [0, 0, 97, 96, 95, 94, 93, 92, 91, 90],
+  [80, 81, 82, 83, 84, 85, 86, 87, 88, 89],
+  [79, 78, 77, 76, 75, 74, 73, 72, 71, 70],
+  [60, 61, 62, 63, 64, 65, 66, 67, 68, 69],
+  [59, 58, 57, 56, 55, 54, 53, 52, 51, 50],
+  [40, 41, 42, 43, 44, 45, 46, 47, 48, 49],
+  [39, 38, 36, 35, 34, 33, 32, 30, 28, 27],
+  [17, 18, 19, 20, 21, 22, 23, 24, 25, 26],
+  [16, 15, 14, 13, 12, 11, 10, 9, 0, 0],
+  [1, 2, 3, 4, 5, 6, 7, 8, 0, 0]
+]
+///peredelat 37 dobavit
+const grid = document.querySelector(".grid");
+log(grid);
+gameField.forEach((row, i) => {
+  log(row);
+  row.forEach((cell, ii) => {
+    grid.innerHTML += `
+    <div id="${cell}" class="cell"></div>
+    `
+  })
+
+});
+
+
+
 const cells = document.querySelectorAll(".cell");
 log(cells);
 cells[91 - 1].innerHTML = `
     <span class="material-symbols-outlined">
          elderly
     </span>
+    <span class="material-symbols-outlined">
+  elderly_woman
+  </span>  
+  <span class="material-symbols-outlined">
+  accessible
+  </span>
+  <span class="material-symbols-outlined">
+  accessible_forward
+  </span>
 `;
+
+cells[91 - 1].classList.add("with-user");
+
+
+const user = {
+  position: 1,
+
+}
+
+
+
+setInterval(() => {
+  user.position++;
+  log(user);
+  moveUser();
+}, 1000)
+
+function moveUser() {
+  const currentCell = document.getElementById(user.position);
+  currentCell.innerHTML += `
+<span class="material-symbols-outlined">
+elderly
+</span>
+`
+}
+
