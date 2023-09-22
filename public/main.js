29ecc4ef5c3606aa8d105d099740695c9b866a48
VladislavVest <vladislavbrusencov@gmail.com>
2023-09-22 18:43:44 +0300

    top panel

--- public/main.js
+++ public/main.js
@@ -1,7 +1,6 @@
 const log = console.log;
 
-
-
+const img = document.createElement('img');
 
 
 const socket = io();
@@ -11,13 +10,13 @@ const socket = io();
 // // });
 
 const gameField = [
-  [0, 0, 97, 96, 95, 94, 93, 92, 91, 90],
-  [80, 81, 82, 83, 84, 85, 86, 87, 88, 89],
-  [79, 78, 77, 76, 75, 74, 73, 72, 71, 70],
-  [60, 61, 62, 63, 64, 65, 66, 67, 68, 69],
-  [59, 58, 57, 56, 55, 54, 53, 52, 51, 50],
-  [40, 41, 42, 43, 44, 45, 46, 47, 48, 49],
-  [39, 38, 36, 35, 34, 33, 32, 30, 28, 27],
+  [0, 0, 96, 95, 94, 93, 92, 91, 90, 90],
+  [79, 80, 81, 82, 83, 84, 85, 86, 87, 88],
+  [78, 77, 76, 75, 74, 73, 72, 71, 70, 69],
+  [59, 60, 61, 62, 63, 64, 65, 66, 67, 68],
+  [58, 57, 56, 55, 54, 53, 52, 51, 50, 49],
+  [39, 40, 41, 42, 43, 44, 45, 46, 47, 48],
+  [38, 37, 36, 35, 34, 33, 32, 30, 28, 27],
   [17, 18, 19, 20, 21, 22, 23, 24, 25, 26],
   [16, 15, 14, 13, 12, 11, 10, 9, 0, 0],
   [1, 2, 3, 4, 5, 6, 7, 8, 0, 0]
@@ -54,21 +53,31 @@ cells[91 - 1].innerHTML = `
   </span>
 `;
 
+cells[89 - 1].innerHTML = `
+<div>
+      <img class="dice" src="./images/dice.gif" alt="">
+     
+    </div>
+`
+
+
+
+
+
 cells[91 - 1].classList.add("with-user");
 
 
 const user = {
   position: 1,
-
 }
 
 
 
-setInterval(() => {
-  user.position++;
-  log(user);
-  moveUser();
-}, 1000)
+// setInterval(() => {
+//   user.position++;
+//   log(user);
+//   moveUser();
+// }, 1000)
 
 function moveUser() {
   const currentCell = document.getElementById(user.position);
